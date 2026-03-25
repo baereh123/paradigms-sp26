44d7ae590c844899bf820fdec6d5eaec3b55b320
Joanna C. S. Santos <jc_joanna@yahoo.com.br>
2026-03-25 15:31:40 -0400

    last changes from demo

--- REST/live_rest_client.py
+++ REST/live_rest_client.py
@@ -4,19 +4,20 @@ import csv
 HOST = "http://jcssdev.pythonanywhere.com/"
 
 
-def main():
-	with open("bugs.csv", "w") as f:
+def main(resource, fields):
+	with open(f"{resource}.csv", "w") as f:
 		csv_writer = csv.writer(f)
 		# csv header
-		csv_writer.writerow(["id", "package", "status", "summary"])
-		url = HOST + "bugs" # page 1
+		csv_writer.writerow(fields)
+		url = HOST + resource # page 1
 		while url:
 			response = requests.get(url)
 			json_resp = response.json()
 			url = json_resp["next"]
 			for r in json_resp["results"]:
-				csv_writer.writerow([r["id"], r["package"], r["status"], r["summary"]])
+				csv_writer.writerow([r[f] for f in fields])
 
 	print("FINISHED!")
 if __name__ == '__main__':
-	main()
+	main("bugs", ["id", "package", "status", "summary"])
+	main("comments", ["id", "bug", "user", "content"])
